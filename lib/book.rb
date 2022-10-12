118ba804688773295a068225f816b9d1c1988674
Astika Luintel <astild2020@gmail.com>
2022-10-12 12:32:46 -0400

    commit tree d12bdacab57ec039a118b94b5a96fbb63d7efa5f

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
+class Book
+    attr_reader :title, :author, :page, :genre
+    attr_writer :page
+
+    def initialized(title, author, page, genre)
+        @title = title
+        @author = author
+        @page = page
+        @genre = genre
+    end
+
+end
+
+newbook = Book.new
+newbook.title = "nd Then There Were None"
+
+
 
 
